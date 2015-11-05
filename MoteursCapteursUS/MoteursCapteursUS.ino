e072fe6870cf068e0a19802cd546231208f62652
Tainder <jeffrey57630@gmail.com>
2015-11-05 14:53:26 +0100

    Programme voiture autonome avec capteurs US devant

--- MoteursCapteursUS/MoteursCapteursUS.ino
+++ MoteursCapteursUS/MoteursCapteursUS.ino
@@ -1,90 +1,63 @@
-  int TRIGG = 12;
-  int ECHOG = 13;
-  int TRIGD = 2;
-  int ECHOD = 3;
-  int MOTEURSG = 6;
-  int MOTEURSD = 5;
-  long distance_gauche,distance_droite;
-  int dist,i;
-  int CONTROLED1 =8;  // Control1 MoteurDroit
-  int CONTROLED2 =7;  // Control2 MoteurDroit
-  int CONTROLEG1 =11;  // Control1 MoteurGauche
-  int CONTROLEG2 =10;  // Control2 MoteurGauche
-  
+#include "CVoiture.h"
 
+// Création de l'object
+CVoiture * voiture;
+  
 void setup() {
-    pinMode(ECHOG, INPUT);
-    pinMode(TRIGG, OUTPUT);
-    pinMode(ECHOD, INPUT);
-    pinMode(TRIGD, OUTPUT);    
-    pinMode(MOTEURSG, OUTPUT);
-    pinMode(MOTEURSD, OUTPUT);
-    pinMode(CONTROLEG1, OUTPUT);
-    pinMode(CONTROLEG2, OUTPUT);
-    pinMode(CONTROLED1, OUTPUT);
-    pinMode(CONTROLED2, OUTPUT);
+      //Création structures
+
+    // Structures moteurs
+    struct Moteur MG;
+    MG.pinControle1 = 11;
+    MG.pinControle2 = 10;
+    MG.pinEnable = 6;
+  
+    struct Moteur MD;
+    MD.pinControle1 = 8;
+    MD.pinControle2 = 7;
+    MD.pinEnable = 5;
+  
+    // Structures capteurs
+    struct CapteurUs CDE;
+    CDE.TRIG = 12;
+    CDE.ECHO = 13;
+  
+    struct CapteurUs CDR;
+    CDR.TRIG = 2;
+    CDR.ECHO = 3;
+  
+    voiture = new CVoiture(MG,MD,CDR,CDE); // Constructeur Moteur gauche, moteur droit, capteur droit , capteur devant
 }
 
 void loop() {
 
+    int distance_gauche ,distance_droite ;
+    distance_gauche=voiture->get_US_Gauche();
+    distance_droite=voiture->get_US_Droite();
 
-    // Calcul distance à gauche
-    digitalWrite(TRIGG,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIGG,LOW);
-    delayMicroseconds(30);
-  
-    distance_gauche=pulseIn(ECHOG, HIGH);
-    distance_gauche=distance_gauche/29/2;
-
-    // Calcul distance à droite
-    
-    digitalWrite(TRIGD,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIGD,LOW);
-    delayMicroseconds(30);
-    distance_droite=pulseIn(ECHOD, HIGH);
-    distance_droite=distance_droite/29/2;
-
+       voiture->avancer(150);
         // On va en marche arrière si obstacle 2 capteurs
-      if (distance_gauche <20 && distance_droite < 20){
-      digitalWrite(MOTEURSD,HIGH);
-      digitalWrite(MOTEURSG,HIGH);
-      
-      digitalWrite(CONTROLED1, LOW);
-      digitalWrite(CONTROLED2, HIGH);
-
-      digitalWrite(CONTROLEG1, LOW);
-      digitalWrite(CONTROLEG2, HIGH);
-
-      delay(1000);
-      }
-    
-    //On tourne sur le coté où le mur est le plus proche sinon on avance
-    if (distance_droite <20){
-        digitalWrite(CONTROLED1, LOW);
-        digitalWrite(CONTROLED2, HIGH);
-            delay (800);
-      }
-    //On tourne sur le coté où le mur est le plus proche sinon on avance
-
-    else if (distance_gauche<20){
-        digitalWrite(CONTROLEG1, LOW);
-        digitalWrite(CONTROLEG2, HIGH);
+    if (distance_gauche <20 && distance_droite < 20){
+        voiture->reculer(200);
         delay(500);
+    }  
+    //On tourne sur le coté où le mur est le plus proche sinon on avance
+    else if (distance_droite <15){
+        voiture->tournerGauche(200,150);
+       
+    }
+    //On tourne sur le coté où le mur est le plus proche sinon on avance
+    else if (distance_gauche<15){
+        voiture->tournerDroite(200,150);
+       
     }
 
     
-    else {
-      digitalWrite(MOTEURSD,HIGH);
-      digitalWrite(MOTEURSG,HIGH);
-      digitalWrite(CONTROLED1, HIGH);
-      digitalWrite(CONTROLED2, LOW);
+   
 
-      digitalWrite(CONTROLEG1, HIGH);
-      digitalWrite(CONTROLEG2, LOW);
-      }
-    
-    
-
+  /*  if (distance_gauche < 20)
+      voiture->avancer(0);
+    else if (distance_droite < 20)
+      voiture->avancer(0);
+    else voiture->avancer(150);*/
 }
